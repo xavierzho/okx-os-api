142039e7a0ffbf31a9c9d9496219236c4851e1be
Xavier <jonescyna@gmail.com>
2025-02-15 18:06:38 +0800

    fix: compiler output

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,23 +4,32 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import {dts} from "rollup-plugin-dts";
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
       file: 'dist/index.cjs',
-      format: 'cjs'
+      format: 'cjs',
+      sourcemap: true
     },
-    {
+    external: ['axios'],
+    plugins: [typescript({tsconfig: './tsconfig.json'}), commonjs(), resolve(), json()]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es'
+    },
+    plugins: [dts()]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
       file: 'dist/index.esm.js',
-      format: 'esm'
+      format: 'es'
     },
-  ],
-  plugins: [
-    json(),
-    typescript({tsconfig: './tsconfig.json'}),
-    resolve(),
-    commonjs(),
-    dts(), // This will generate .d.ts files
-  ]
-}
+    external: ['axios', 'node:crypto'],
+    plugins: [typescript({tsconfig: './tsconfig.json'}), json()]
+  }
+]
